83751e9abd66a5a242a85861586c692007e49ec7
shaurya karwal <karwalshaurya@gmail.com>
2026-04-22 22:20:05 +1200

    Clerk with design

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -2,8 +2,11 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
-            <SignIn />
+        <div className="flex min-h-screen items-center justify-center bg-primary/10">
+            <div className="flex flex-col items-center gap-20">
+                <h1 className="text-7xl">PennyWise</h1>
+                <SignIn />
+            </div>
         </div>
     );
 }
